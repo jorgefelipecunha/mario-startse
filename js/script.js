95071b012a4b3758c9663dc4fbaf8a3412f5e4f6
José Ronaldo <dev.joseronaldo@gmail.com>
2022-06-18 17:32:45 -0300

    :sparkles: feat: Trocando sprites do personagem principal e arrumando suas animações e transiçoes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-const mario = document.querySelector(".super-mario");
-const pipe = document.querySelector(".pipe-game");
+const mario = document.querySelector('.super-mario')
+const pipe = document.querySelector('.pipe-game')
+
+var gameover = false
 
 const jump = () => {
-  mario.classList.add("jump-mario");
-
+  if (!gameover) {
+    mario.classList.add('jump-mario')
+    mario.src = './Images/ninja_man_jump.gif'
+  }
   setTimeout(() => {
-    mario.classList.remove("jump-mario");
-  }, 500);
-};
+    mario.classList.remove('jump-mario')
+
+    if (!gameover) {
+      mario.src = './Images/ninja_man_run.gif'
+    }
+  }, 500)
+}
 
 const loopGame = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+  const pipePosition = pipe.offsetLeft
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
+    console.log(marioPosition)
+    pipe.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+    mario.style.animation = 'none'
+    mario.style.bottom = `${marioPosition}px`
 
-    mario.src = "./Images/mario-game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "45px";
+    gameover = true
+    mario.src = './Images/ninja_man_dead.gif'
+    mario.style.width = '115px'
+    mario.style.marginLeft = '20px'
 
-    clearInterval(loopGame);
+    clearInterval(loopGame)
+    gameover = true
   }
-}, 10);
+}, 10)
 
-document.addEventListener("keydown", jump);
+document.addEventListener('keydown', jump)
