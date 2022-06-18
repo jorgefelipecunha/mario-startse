8cb7acd96607422b0676e2454bdec0eb9b04a31c
Beto Camin <beto@cazulo.com>
2022-06-18 12:43:56 -0300

    adiciona sons jump e gameover

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
+const marioJumpSound = new Audio('../sounds/smb_jump-small.mp3');
+const gameOver = new Audio('../sounds/smb_gameover.mp3');
 
 const jump = () => {
   mario.classList.add("jump-mario");
+  marioJumpSound.play();
 
   setTimeout(() => {
     mario.classList.remove("jump-mario");
@@ -16,12 +19,13 @@ const loopGame = setInterval(() => {
     .bottom.replace("px", "");
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    gameOver.play();
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
-
+       
     mario.src = "./Images/mario-game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
