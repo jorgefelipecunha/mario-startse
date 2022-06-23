9f1efcf614701cc535a4e88df2e2e4a7444012fc
Thiago Souza <thisouza.dev@gmail.com>
2022-06-23 00:06:45 -0300

    visual updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
+const clouds = document.querySelector(".cloud-game");
+const ground = document.querySelector(".ground-game");
+const trees = document.querySelector(".trees");
+const mountain = document.querySelector(".mountain-game");
+const bgclouds= document.querySelector(".bg-clouds");
+const titlescreen = document.querySelector(".title-screen");
+
+//TITLE-SCREEN
+
+//.title-screen-hidden  classe para esconder  a title screen
+
+const titleopacity = () => {
+  titlescreen.classList.add("title-screen-hidden");
+  pipe.classList.remove("animation-stop");
+}
+
+
 
 const jump = () => {
   mario.classList.add("jump-mario");
@@ -26,8 +43,19 @@ const loopGame = setInterval(() => {
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
 
+    clouds.classList.add("animation-stop");
+
+    ground.classList.add("animation-stop");
+
+    trees.classList.add("animation-stop");
+
+    mountain.classList.add("animation-stop");
+
+    bgclouds.classList.add("animation-stop");
+    
     clearInterval(loopGame);
   }
 }, 10);
 
+document.addEventListener("keydown", titleopacity); 
 document.addEventListener("keydown", jump);
