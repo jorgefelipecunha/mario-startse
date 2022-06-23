52ab840042c1455446f7c56d7778f0eadc73425a
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-06-23 01:32:14 -0300

    game-script-var-and-const-updated

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var & const
+
+const mario = document.querySelector(".super-mario");
+const pipe = document.querySelector(".pipe-form");
+const theme = document.querySelector(".theme-song");
+const restart = document.querySelector(".restart");
+const jumpSong = document.querySelector(".jump-song");
+const ingameScore = document.querySelector(".ingame");
+const levelDisplay = document.querySelector(".level");
