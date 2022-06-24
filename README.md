fa01767781d91f5b323b602155f5e3639d02386b
Daianne Cordeiro <daianne.nc@gmail.com>
2022-06-24 12:38:08 -0300

    Submit do Jogo criado na aula

---
+++ README.md
@@ -0,0 +1,25 @@
+<div align="center">
+  <img align="right" height="150" src="https://github.com/daiannecordeiro/witch-in-the-woods/blob/main/assets/img/Witch/witch1.gif?raw=true">
+  <img align="left" height="150" src="https://github.com/daiannecordeiro/witch-in-the-woods/blob/main/assets/img/Witch/witch1.gif?raw=true">
+  <h1>✨ WITCH IN THE WOODS ✨</h1>
+  <h3>Jogo criado no curso Tech Academy da Startse</h3>
+</div> 
+<div align="center">
+  <a align="center" href="https://daiannecordeiro.github.io/witch-in-the-woods/">JOGAR</a>
+</div>
+
+##
+<div align="center">
+  <h1>Agradecimentos e Créditos</h1>
+  <br>
+  <p>Um agradecimento aos professores da StartSe, em especial aos professores Grabriel, Rafael,</p>
+  <p>Jorge e Fernanda que ministraram a aula que auxiliou na criação deste jogo</p>
+  <br>
+  <p>Créditos para a arte do background: <a href="https://edermunizz.itch.io/">Eder Muniz</a></p>
+  <br>
+  <p>Créditos para a música: <a href="https://www.chosic.com/download-audio/29628/">The Lone Wolf - Alexander Nakarada</a></p>
+  <br>
+  <p>Créditos para a FSX: <a href="https://mixkit.co/free-sound-effects/">MixKit</a> e <a href="https://www.dreamstime.com/sound-horror-noise-scared-girl-scream-usable-game-movie-distance-mid-acoustic-processed-scared-girl-scream-audio176546478">DreamStime</a> </p>
+  <br>
+  <p>Créditos para a arte da personagem principal: <a href="https://www.behance.net/ilustradai">Daianne Cordeiro</a> </p>
+</div>
--- js/script.js
+++ js/script.js
@@ -1,33 +1,75 @@
-const mario = document.querySelector(".super-mario");
-const pipe = document.querySelector(".pipe-game");
+const witch = document.querySelector('.witch');
+const pumpkin = document.querySelector('.pumpkin');
+const thorn = document.querySelector('.thorn');
+const gameOver = document.querySelector('.game-over');
+const score = document.querySelector('.score');
 
-const jump = () => {
-  mario.classList.add("jump-mario");
+let pontos = 0;   
 
-  setTimeout(() => {
-    mario.classList.remove("jump-mario");
-  }, 500);
-};
+const layer0 = document.querySelector("#layer0");
+const layer1 = document.querySelector("#layer1");
+const layer2 = document.querySelector("#layer2");
+const layer3 = document.querySelector("#layer3");
+const layer4 = document.querySelector("#layer4");
+const layer5 = document.querySelector("#layer5");
+const layer6 = document.querySelector("#layer6");
+
+const audioGameOver = new Audio('../assets/Sound/mixkit-fairytale-game-over-1945.wav');
+const audioScream = new Audio('../assets/Sound/scream.mp3');
+const audioJump = new Audio('../assets/Sound/mixkit-quick-jump-arcade-game-239.wav') 
+
+const pulo = () => {
+    witch.classList.add("pulo");
+
+    setTimeout(() => {
+        witch.classList.remove("pulo");
+    }, 700)
+
+    audioJump.play();
+ }
+
+document.addEventListener("keydown", pulo)
+document.addEventListener("click", pulo)
 
 const loopGame = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+  const pumpkinPosition = pumpkin.offsetLeft;
+  const thornPosition = thorn.offsetLeft;
+  const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
+    
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+  if ((pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) || (thornPosition <= 180 && thornPosition > 0 && witchPosition < 80)) {
+    pumpkin.style.animation = "none";
+    pumpkin.style.left = `${pumpkinPosition}px`;
 
-    mario.src = "./Images/mario-game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "45px";
+    thorn.style.animation = "none";
+    thorn.style.left = `${thornPosition}px`;
+
+    witch.style.animation = "none";
+    witch.style.bottom = `${witchPosition}px`;
+        
+    witch.src = "../assets/img/Witch/witch-dead.gif";
+    witch.style.width = "358px";
+    witch.style.height = "490px";
+    witch.style.marginLeft = "45px";
+
+    layer0.style.animation = "none";
+    layer1.style.animation = "none";
+    layer2.style.animation = "none";
+    layer3.style.animation = "none";
+    layer4.style.animation = "none";
+    layer5.style.animation = "none";
+    layer6.style.animation = "none";
+
+    gameOver.style.display = "block";
+
+    audioScream.play();
+    audioGameOver.play();
 
     clearInterval(loopGame);
-  }
-}, 10);
 
-document.addEventListener("keydown", jump);
+  } else {
+    pontos += 0.04  ;
+    score.innerHTML = `${pontos.toFixed(0)}`
+  }
+},10)
