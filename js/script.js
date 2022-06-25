36a0a208482767092a0aa2a9a653f20134944f58
Maycon <mayconl.aguiar@gmail.com>
2022-06-25 21:56:22 +0044

    exercicio Mario

--- js/script.js
+++ js/script.js
@@ -4,30 +4,39 @@ const pipe = document.querySelector(".pipe-game");
 const jump = () => {
   mario.classList.add("jump-mario");
 
-  setTimeout(() => {
+  setTimeout(() =>{
     mario.classList.remove("jump-mario");
   }, 500);
 };
 
-const loopGame = setInterval(() => {
+const loopGame = setInterval(() => { 
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+  .getComputedStyle(mario)
+  .bottom.replace("px", "");
+
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
-
-    mario.style.animation = "none";
+    
+    mario.style.left = "none";
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = "./Images/mario-game-over.png";
+    mario.src = "./images/mario-game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
 
-    clearInterval(loopGame);
-  }
+    clearInterval(loopGame)
+
+    }
+
 }, 10);
 
 document.addEventListener("keydown", jump);
+
+
+
+
+
+
