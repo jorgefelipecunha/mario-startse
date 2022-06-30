5540d296c9ce9e4f5787bb7b07da6ef69d9e530a
Daianne Cordeiro <daianne.nc@gmail.com>
2022-06-30 16:58:49 -0300

    fix bug

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const layer4 = document.querySelector("#layer4");
 const layer5 = document.querySelector("#layer5");
 const layer6 = document.querySelector("#layer6");
 
-const audioGameOver = new Audio('../assets/sound/mixkit-fairytale-game-over-1945.wav');
-const audioScream = new Audio('../assets/sound/scream.mp3');
-const audioJump = new Audio('../assets/sound/mixkit-quick-jump-arcade-game-239.wav') 
+const audioGameOver = new Audio('assets/sound/mixkit-fairytale-game-over-1945.wav');
+const audioScream = new Audio('assets/sound/scream.mp3');
+const audioJump = new Audio('assets/sound/mixkit-quick-jump-arcade-game-239.wav') 
 const audioBack = document.querySelector('.music');
 
 
@@ -52,44 +52,45 @@ function startGame() {
   scoreArea.style.display = "block";
   start.style.display = "none";
 
-const loopGame = setInterval(() => {
+  const loopGame = setInterval(() => {
 
-  const pumpkinPosition = pumpkin.offsetLeft;
-  const thornPosition = thorn.offsetLeft;
-  const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
-    
-  if ((pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) || (thornPosition <= 180 && thornPosition > 0 && witchPosition < 80)) {
-    pumpkin.style.animation = "none";
-    pumpkin.style.left = `${pumpkinPosition}px`;
+    const pumpkinPosition = pumpkin.offsetLeft;
+    const thornPosition = thorn.offsetLeft;
+    const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
+      
+    if ((pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) || (thornPosition <= 180 && thornPosition > 0 && witchPosition < 80)) {
+      pumpkin.style.animation = "none";
+      pumpkin.style.left = `${pumpkinPosition}px`;
 
-    thorn.style.animation = "none";
-    thorn.style.left = `${thornPosition}px`;
+      thorn.style.animation = "none";
+      thorn.style.left = `${thornPosition}px`;
 
-    witch.style.animation = "none";
-    witch.style.bottom = `${witchPosition}px`;
-        
-    witch.src = "../assets/img/witch/witchdead.gif";
-    witch.style.width = "250px";
-    witch.style.height = "343px";
-    witch.style.marginLeft = "45px";
+      witch.style.animation = "none";
+      witch.style.bottom = `${witchPosition}px`;
+          
+      witch.src = "assets/img/witch/witchdead.gif";
+      witch.style.width = "250px";
+      witch.style.height = "343px";
+      witch.style.marginLeft = "45px";
 
-    layer0.style.animation = "none";
-    layer1.style.animation = "none";
-    layer2.style.animation = "none";
-    layer3.style.animation = "none";
-    layer4.style.animation = "none";
-    layer5.style.animation = "none";
-    layer6.style.animation = "none";
+      layer0.style.animation = "none";
+      layer1.style.animation = "none";
+      layer2.style.animation = "none";
+      layer3.style.animation = "none";
+      layer4.style.animation = "none";
+      layer5.style.animation = "none";
+      layer6.style.animation = "none";
 
-    gameOver.style.display = "block";
+      gameOver.style.display = "block";
 
-    audioScream.play();
-    audioGameOver.play();
+      audioScream.play();
+      audioGameOver.play();
 
-    clearInterval(loopGame);
+      clearInterval(loopGame);
 
-  } else {
-    pontos += 0.04  ;
-    score.innerHTML = `${pontos.toFixed(0)}`
-  }
-},10)}
+    } else {
+      pontos += 0.04  ;
+      score.innerHTML = `${pontos.toFixed(0)}`
+    }
+  },10)
+}
