968803581474efc7f84d6c595544ea0dd3e4946b
Felipe Ribeiro <felipeonlinebr@gmail.com>
2022-07-02 15:44:57 -0300

    Modificação dos sprites

---
+++ README.md
@@ -0,0 +1,12 @@
+Olá!
+
+Antes de tudo,
+já se hidratou hoje?
+Se sim, pode continuar.
+
+Como modifiquei
+<h1>Como modifiquei:</h1>
+
+A tela de game over e o BG foram feitos usando imagens pegas de "https://www.spriters-resource.com/snes/smarioworld/sheet/137311", ja a fonte veio de "https://www.cdnfonts.com/super-mario-world-text-box.font".
+
+Usei o Aseprite para deixar do jeito que eu precisava.
--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
+const bg = document.querySelector(".game");
+const gameOver = document.querySelector(".gameOver");
+
+bg.style.backgroundPositionX = 0;
+const ground = document.querySelector(".ground");
+ground.style.backgroundPositionX = 0;
+
+var pipePosition = pipe.offsetLeft;
+var marioPosition =+ window
+.getComputedStyle(mario)
+  .bottom.replace("px", "");
+
+
 
 const jump = () => {
   mario.classList.add("jump-mario");
@@ -9,25 +22,53 @@ const jump = () => {
   }, 500);
 };
 
+document.addEventListener('keydown', jump);
+
 const loopGame = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+  MoveBG();
+  MoveGround();
+  CheckIfCollide();
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
-
-    mario.src = "./Images/mario-game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "45px";
-
-    clearInterval(loopGame);
-  }
 }, 10);
 
-document.addEventListener("keydown", jump);
+function MoveBG() {
+  let x = Number.parseInt(bg.style.backgroundPositionX);
+  x -= 1;
+  bg.style.backgroundPositionX = `${x}px`;
+}
+function MoveGround() {
+  let x = Number.parseInt(ground.style.backgroundPositionX);
+  x -= 10;
+  ground.style.backgroundPositionX = `${x}px`;
+}
+
+function CheckIfCollide() {
+
+  marioPosition =+ window
+  .getComputedStyle(mario)
+    .bottom.replace("px", "");
+  
+  pipePosition = pipe.offsetLeft;
+
+  /*Pipe*/
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    GameStateLose();
+  }
+}
+
+function GameStateLose() {
+  pipe.style.animation = "none";
+  pipe.style.left = `${pipePosition}px`;
+
+  mario.style.animation = "none";
+  mario.style.bottom = `${marioPosition}px`;
+
+  mario.src = "./Images/mario-game-over.png";
+  mario.style.width = "60px";
+  mario.style.marginLeft = "45px";
+
+  gameOver.style.display = "flex";
+
+  clearInterval(loopGame);
+}
