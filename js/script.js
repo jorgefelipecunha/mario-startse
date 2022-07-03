c7d8c734a7c7bb4f39b70ed6cfae69e05b7fefef
Sandra Ferla <artkaviah@gmail.com>
2022-07-02 22:07:37 -0300

    mudança de personagens e inserção de mais 2 obstáculos

--- js/script.js
+++ js/script.js
@@ -1,33 +1,62 @@
-const mario = document.querySelector(".super-mario");
-const pipe = document.querySelector(".pipe-game");
+const lula = document.querySelector(".lula");
+const tebett = document.querySelector(".tebett");
+const ciro = document.querySelector(".ciro");
+const bolsonaro = document.querySelector(".bolsonaro");
+const nuvens = document.querySelector(".cloud-game");
+const game = document.querySelector(".game");
+const gameOver = document.querySelector(".game-over")
 
 const jump = () => {
-  mario.classList.add("jump-mario");
+  lula.classList.add("jump-lula");
 
   setTimeout(() => {
-    mario.classList.remove("jump-mario");
-  }, 500);
+    lula.classList.remove("jump-lula");
+  }, 800);
 };
 
 const loopGame = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
+  const tebettPosition = tebett.offsetLeft;
+  const ciroPosition = ciro.offsetLeft;
+  const bolsonaroPosition = bolsonaro.offsetLeft;
+  const lulaPosition = +window
+    .getComputedStyle(lula)
     .bottom.replace("px", "");
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+  if ((tebettPosition <= 120 && tebettPosition > 0 && lulaPosition < 80) || (ciroPosition <= 120 && ciroPosition > 0 && lulaPosition < 80)) {
+    tebett.style.animation = "none";
+    tebett.style.left = `${tebettPosition}px`;
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+    ciro.style.animation = "none";
+    ciro.style.left = `${ciroPosition}px`;
 
-    mario.src = "./Images/mario-game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "45px";
+    lula.style.animation = "none";
+    lula.style.bottom = `${lulaPosition}px`;
+
+    lula.src = "./Images/the-end.gif";
+    // lula.style.width = "75px";
+    lula.style.marginLeft = "45px";
+    lula.style.bottom = "0";
 
     clearInterval(loopGame);
-  }
+    } else if (bolsonaroPosition <= 120 && bolsonaroPosition > 0 && lulaPosition < 80) {
+      tebett.src ="";
+      ciro.src ="";
+      bolsonaro.src ="";
+      nuvens.src ="";
+      game.style.borderBottom = "none";
+      game.style.background = "linear-gradient(#b41d1d, #f08c8c)";
+
+      gameOver.style.display = "flex";
+
+      lula.style.animation = "none";
+      lula.style.bottom = `${lulaPosition}px`;
+
+      lula.src = "./Images/the-end.gif";
+      lula.style.width = "200px";
+      lula.style.marginLeft = "45px";
+
+      clearInterval(loopGame);
+    }
 }, 10);
 
 document.addEventListener("keydown", jump);
