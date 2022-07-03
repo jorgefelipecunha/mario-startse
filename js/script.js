97041f0948f6b75e66f45c2c28bef8bd9127ecbd
Gabriel Aparecido da paixão <gabriel711906@hotmail.com>
2022-07-02 23:43:01 -0300

    Recriando projeto mario

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
+
+let musica=document.getElementById("musica");
+let pulo=document.getElementById("pulo");
+let morte=document.getElementById("morte");
+let fim=document.getElementById("somGameover");
+
+const gameOver = document.querySelector('.over, .hidden')
+const btn = document.querySelector('#button')
+
+const score = document.querySelector('.score');
+var count = 0;
+const time = setInterval(() => {
+  count++;
+
+  document.getElementById('count').innerHTML = count;
+}, 1);
+
+
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
 
+musica.play();
+
 const jump = () => {
   mario.classList.add("jump-mario");
+  pulo.play(); 
 
   setTimeout(() => {
     mario.classList.remove("jump-mario");
@@ -15,7 +36,7 @@ const loopGame = setInterval(() => {
     .getComputedStyle(mario)
     .bottom.replace("px", "");
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+  if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
@@ -25,9 +46,36 @@ const loopGame = setInterval(() => {
     mario.src = "./Images/mario-game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
+    morte.play();
+    musica.pause();
+    
+  
+    
+    const loop2 = setInterval(() => {
+      document.getElementById('countEnd').innerHTML = count;
+  })
+
+    clearInterval(time);
 
     clearInterval(loopGame);
+
+    gameOver.classList.add('over')
+    gameOver.classList.remove('hidden')
+
+    btn.classList.add('over')
+    btn.classList.remove('hidden')
+
+    
+    btn.addEventListener('click', function() {
+
+      location.reload()
+    })
   }
+  
 }, 10);
 
+
 document.addEventListener("keydown", jump);
+document.addEventListener('click', jump);
+
+
