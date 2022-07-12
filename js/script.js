de0de80839af280b6ec8170914021aff835206fc
Sandra Ferla <artkaviah@gmail.com>
2022-07-12 17:41:23 -0300

    refatoramento

--- js/script.js
+++ js/script.js
@@ -6,10 +6,15 @@ const nuvens = document.querySelector(".cloud-game");
 const game = document.querySelector(".game");
 const gameOver = document.querySelector(".game-over");
 const playAgain = document.querySelector(".play-again");
-const botaoClicado = document.getElementById("try-again");
+const botaoRestart = document.getElementById("#restart-game");
 const planalto = document.querySelector(".planalto");
 const jumpLula = document.querySelector('.jump-lula');
-
+const win = document.querySelector('.win');
+const granFinale = document.querySelector('.gran-finale');
+const bandeira = document.querySelector('.bandeira');
+const startContainer = document.querySelector('.start');
+const botaoStart = document.getElementById("#start-game");
+const fimContainer = document.querySelector(".fim");
 
 const jump = () => {
   lula.classList.add("jump-lula");
@@ -19,96 +24,83 @@ const jump = () => {
   }, 600);
 };
 
-
-
 const loopGame = setInterval(() => {
+
   const tebettPosition = tebett.offsetLeft;
   const ciroPosition = ciro.offsetLeft;
   const bolsonaroPosition = bolsonaro.offsetLeft;
-  const planaltoPosition = planalto.offsetRight;
-  const lulaPosition = lula.offsetLeft;
   const lulaBottomPosition = +window
-    .getComputedStyle(lula)
-    .bottom.replace("px", "");
+  .getComputedStyle(lula)
+  .bottom.replace("px", "");
+
 
   if ((tebettPosition <= 100 && tebettPosition > 0 && lulaBottomPosition < 80) || (ciroPosition <= 120 && ciroPosition > 0 && lulaBottomPosition < 80)) {
+    
     tebett.style.animation = "none";
-    tebett.style.left = `${tebettPosition}px`;
+    // tebett.style.left = `${tebettPosition}px`;
 
     ciro.style.animation = "none";
-    ciro.style.left = `${ciroPosition}px`;
-
     bolsonaro.style.animation = "none";
-    bolsonaro.style.left = `${bolsonaroPosition}px`;
-
-    planalto.style.animation = "none";
-
-    lula.style.animation = "none";
-    lula.style.bottom = `${lulaBottomPosition}px`;
-
     lula.src = "./Images/the-end.gif";
-    lula.style.width = "300px";
-    lula.style.bottom = "0";
+
+    setTimeout(() => {
+      playAgain.style.display = "flex";
+    },1500);
+
+  clearInterval(loopGame);
+
+
+  } else if (bolsonaroPosition <= 120 && bolsonaroPosition > 0 && lulaBottomPosition < 80) {
+    // nuvens.src ="";
+    gameOver.style.display = "flex";
+    // planalto.src ="";
+    lula.src = "./Images/the-end.gif";
 
     setInterval(() => {
       playAgain.style.display = "flex";
       lula.src = "";
-      tebett.src = "";
-      ciro.src = "";
-      bolsonaro.src = "";
-    },1500);
+    },3000);
 
     clearInterval(loopGame);
 
-    } else if (bolsonaroPosition <= 120 && bolsonaroPosition > 0 && lulaBottomPosition < 80) {
-      nuvens.src ="";
-      gameOver.style.display = "flex";
-
-      lula.src = "./Images/the-end.gif";
-      lula.style.width = "300px";
-      lula.style.bottom = "0";
-      lula.style.right = '100px';
-
-      setInterval(() => {
-        playAgain.style.display = "flex";
-        lula.src = "";
-      },3000);
-
-      clearInterval(loopGame);
-
-    } else if(bolsonaroPosition <= 10 && tebettPosition > 0) {
+    } else if (ciroPosition < 0 && bolsonaroPosition < 0){
       
       lula.classList.add("win");
-      jumpLula.style.animation = 'none';
-
-      setInterval(() => {
-        playAgain.style.display = "flex";
-        nuvens.src = "./Images/fireworks.gif";
-        lula.style.animation = 'none';
-        lula.style.left = `${lulaPosition}px`;
-        lula.style.width = '300px';
-        lula.style.bottom = '200px';
-
-        planalto.style.animation = 'none';
-        
-      },2000);
-
-      setInterval(() => {
-        playAgain.style.display = "flex";
-        lula.src = "";
-        tebett.src = "";
-        ciro.src = "";
-        bolsonaro.src = "";
-      },1500);
+      planalto.style.animation = "planalto-animation 5s linear";
 
 
-    
+      setTimeout(() => {
+        lula.classList.replace("win","gran-finale");
+        lula.style.left = '40%';
+        lula.style.width ='350px';
+        planalto.src = '';
+        nuvens.style.animation = 'none';
+        nuvens.style.left = '40%'
+        nuvens.src = './Images/fireworks.gif';
+        nuvens.style.width = '500px'
+        bandeira.style.animation = 'bandeira 6s';
+        bandeira.style.width = '450px';
+      },4000);
+
+      setTimeout(() => {
+        fimContainer.style.display = "flex";
+      },5000);
 
       clearInterval(loopGame);
-      } 
-
-
+    };
 
 }, 10);
 
-document.addEventListener("keydown", jump);
+const start = () => {
+  lula.src = "./Images/lula.gif";
+  document.addEventListener("keydown", jump);
+  tebett.style.animation = "fig-animation 6s linear";
+  ciro.style.animation = "fig-animation 6s linear 2s";
+  bolsonaro.style.animation = "fig-animation 6s linear 4s";
+  planalto.style.animation = "planalto-animation 5s linear 8s";
+  startContainer.style.display = "none";
+  playAgain.style.display = "none";
+  gameOver.style.display = "none";
+  
+  loopGame();
+};
